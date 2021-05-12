858c888bb38e49e0328b6292b91e5ccc1c0651ae
NiketaJen <niketa.jenerette@gmail.com>
2021-05-12 14:02:41 -0400

    complete brute force

--- twoSums.js
+++ twoSums.js
@@ -4,10 +4,15 @@
  * @return {number[]}
  */
  var twoSum = function(nums, target) {
-    let output = []
-    let i = 0;
     
-    for (i = 0; i < nums.length; i++) {
-        
+    for (let i = 0; i < nums.length; i++) {
+        for(let j = i + 1; j < nums.length; j++){
+            if(nums[i] + nums[j] === target) {
+                return [i , j]
+            }
+        }  
     }
 };
+
+nums = [2,7,11,15]
+target = 9
