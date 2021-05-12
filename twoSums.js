d1a11e29e3e2c6da27bf76ee6f5bc476fab5ec1f
NiketaJen <niketa.jenerette@gmail.com>
2021-05-11 23:54:45 -0400

    version 1

---
+++ twoSums.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ var twoSum = function(nums, target) {
+    let output = []
+    let i = 0;
+    
+    for (i = 0; i < nums.length; i++) {
+        
+    }
+};
